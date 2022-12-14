53122207aea4497165a71d0073cefafb6ca51706
Lao214 <112752790+Lao214@users.noreply.github.com>
2022-12-14 09:52:36 +0800

    浏览器兼容

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,88 +1,42 @@
 <template>
-  <div id="page">
-    <!-- 截图区域 -->
-    <div class="content" ref="imageDom">
-      这里是丰富的网页内容...
-    </div>
-    <!-- 截图区域2 -->
-    <!-- 点击调用方法获取截图 -->
-    <button class="btn" @click="getPrintScreen">获取截图</button>
-    <!-- <div class="img-box"> -->
-      <!-- <h2>截图结果:</h2> -->
-      <!-- 通过img标签把获取到的截图呈现出来 -->
-      <!-- <img :src="imgUrl" alt="" /> -->
-    <!-- </div> -->
+  <div>
+      <div style="padding:50px;width:500px;border:1px solid red;" id="test">
+          <p>我是p标签</p>
+          <h1>我是h1标签</h1>
+          <div>我是div标签</div>
+          <!-- <img src="./11" style="width:200px;" /> -->
+      </div>
+      <el-button type="primary" @click="createImage">生成图片</el-button>
+      <div style="width:500px;height:200px;margin-top:20px;border:1px solid red;">
+          <p>生成的图片</p>
+          <img :src="dataUrl" style="width:200px;" />
+      </div>
   </div>
 </template>
 
 <script>
-// 引入 html2canvas
-import html2canvas from "html2canvas";
-
+import domtoimage from 'dom-to-image'
+import FileSaver from 'file-saver';
 export default {
-  name: "Screenshot",
   data() {
-    return {
-      imgUrl: null, //截图地址
-    };
+      return {
+          dataUrl: ''
+      }
   },
   methods: {
-    //获取截图方法
-    getPrintScreen() {
-      html2canvas(this.$refs.imageDom,
-      {
-        imageTimeout: 15000, //newline
-        scale:3, //newline
-        useCORS: true, //图片跨域，开启跨域配置
-        logging: false,//日志开关，便于查看html2canvas的内部执行流程
-        taintTest: false,//是否在渲染前测试图片
-      }).then(canvas => {
-        // 转成图片，生成图片地址
-        let imgUrl = canvas.toDataURL("image/png",1); //可将 canvas 转为 base64 格式
-        let eleLink = document.createElement("a");
-        eleLink.href = imgUrl; // 转换后的图片地址
-        eleLink.download = "名称";
-        document.body.appendChild(eleLink);
-        eleLink.click();
-        document.body.removeChild(eleLink);
-      });
-    }
-  },
-};
-</script>
-
-
-<!-- 本demo样式代码（不重要） -->
-<style scoped>
-.content {
-    height: 30%;
-    font-size: 26px;
-    font-family: Source Han Sans CN;
-    color: rgb(245, 245, 245);
-    text-align: center;
-    line-height: 200px;
-    background-color: rgb(243, 161, 152);
+      createImage() {
+          let node = document.getElementById('test');
+          let that = this
+          domtoimage.toPng(node)
+              .then(function (dataUrl) {
+                  console.log(dataUrl)
+                  that.dataUrl = dataUrl
+                  FileSaver.saveAs(dataUrl, 'a.png');
+              })
+              .catch(function (error) {
+                  console.error('生成失败', error);
+              });
+      },
   }
-  .btn {
-    display: block;
-    width: 80vw;
-    height: 11.733vw;
-    background: #79c76f;
-    border-radius: 2vw;
-    border: none;
-    font-size: 4.8vw;
-    font-family: Source Han Sans CN;
-    font-weight: 400;
-    color: #ffffff;
-    line-height: 11.733vw;
-    letter-spacing: 4px;
-    text-align: center;
-    margin: 30px 0 20px 40px;
-  }
-#page {
-  padding: 0;
-  width: 100%;
-  height: 100%;
-  background-color: #fff;
 }
-</style>
+</script>
---
+++ src/components/HelloWorld2.vue
@@ -0,0 +1,88 @@
+<template>
+  <div id="page">
+    <!-- 截图区域 -->
+    <div class="content" ref="imageDom">
+      这里是丰富的网页内容...
+    </div>
+    <!-- 截图区域2 -->
+    <!-- 点击调用方法获取截图 -->
+    <button class="btn" @click="getPrintScreen">获取截图</button>
+    <!-- <div class="img-box"> -->
+      <!-- <h2>截图结果:</h2> -->
+      <!-- 通过img标签把获取到的截图呈现出来 -->
+      <!-- <img :src="imgUrl" alt="" /> -->
+    <!-- </div> -->
+  </div>
+</template>
+
+<script>
+// 引入 html2canvas
+import html2canvas from "html2canvas";
+
+export default {
+  name: "Screenshot",
+  data() {
+    return {
+      imgUrl: null, //截图地址
+    };
+  },
+  methods: {
+    //获取截图方法
+    getPrintScreen() {
+      html2canvas(this.$refs.imageDom,
+      {
+        imageTimeout: 15000, //newline
+        scale:3, //newline
+        useCORS: true, //图片跨域，开启跨域配置
+        logging: false,//日志开关，便于查看html2canvas的内部执行流程
+        taintTest: true,//是否在渲染前测试图片
+      }).then(canvas => {
+        // 转成图片，生成图片地址
+        let imgUrl = canvas.toDataURL("image/png",1.0); //可将 canvas 转为 base64 格式
+        let eleLink = document.createElement("a");
+        eleLink.href = imgUrl; // 转换后的图片地址
+        eleLink.download = "名称";
+        document.body.appendChild(eleLink);
+        eleLink.click();
+        document.body.removeChild(eleLink);
+      },300);
+    }
+  },
+};
+</script>
+
+
+<!-- 本demo样式代码（不重要） -->
+<style scoped>
+.content {
+    height: 30%;
+    font-size: 26px;
+    font-family: Source Han Sans CN;
+    color: rgb(245, 245, 245);
+    text-align: center;
+    line-height: 200px;
+    background-color: rgb(243, 161, 152);
+  }
+  .btn {
+    display: block;
+    width: 80vw;
+    height: 11.733vw;
+    background: #79c76f;
+    border-radius: 2vw;
+    border: none;
+    font-size: 4.8vw;
+    font-family: Source Han Sans CN;
+    font-weight: 400;
+    color: #ffffff;
+    line-height: 11.733vw;
+    letter-spacing: 4px;
+    text-align: center;
+    margin: 30px 0 20px 40px;
+  }
+#page {
+  padding: 0;
+  width: 100%;
+  height: 100%;
+  background-color: #fff;
+}
+</style>
