38b61d49de2c3a87efa4c79b3c10999e8beb9617
Lao214 <112752790+Lao214@users.noreply.github.com>
2022-12-15 11:38:21 +0800

    下载图片测试

--- src/App.vue
+++ src/App.vue
@@ -2,8 +2,8 @@
   <div id="app">
 
     <!-- <PageThree></PageThree> -->
-    <main-view></main-view>
-    <!-- <hello-world></hello-world> -->
+    <!-- <main-view></main-view> -->
+    <hello-world></hello-world>
   </div>
 </template>
 
--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,90 +1,93 @@
 <template>
-  <div id="page">
-    <!-- 截图区域 -->
-    <div class="content" ref="imageDom">
-      这里是丰富的网页内容...
+  <div class="body">
+    <div class="childSwiper" ref="imageDom">
+       <swiper :options="swiperOption" ref="mySwiper" >
+         <swiper-slide class="one">12312312312</swiper-slide>
+         <swiper-slide class="two">2</swiper-slide>
+         <swiper-slide class="three">3</swiper-slide>
+         <swiper-slide class="four">4</swiper-slide>
+         <!-- <div class="swiper-pagination" slot="pagination"></div> -->
+         <!-- <div class="swiper-button-prev" slot="button-prev"> <i class="el-icon-caret-left"></i> </div>
+         <div class="swiper-button-next" slot="button-next"> <i class="el-icon-caret-right"></i></div> -->
+       </swiper>
     </div>
-    <!-- 截图区域2 -->
-    <!-- 点击调用方法获取截图 -->
     <button class="btn" @click="getPrintScreen">获取截图</button>
-    <div class="img-box">
-      <h2>截图结果:</h2>
-      <!-- 通过img标签把获取到的截图呈现出来 -->
-      <img :src="imgUrl" alt="" />
-    </div>
   </div>
 </template>
 
 <script>
-// 引入 html2canvas
+import {swiper,swiperSlide} from 'vue-awesome-swiper'
+import 'swiper/dist/css/swiper.css'
 import html2canvas from "html2canvas";
-import FileSaver from 'file-saver'
-
+import FileSaver from 'file-saver';
 export default {
-  name: "Screenshot",
-  data() {
-    return {
-      imgUrl: null, //截图地址
-    };
-  },
-  methods: {
+    components:{swiper,swiperSlide},
+    data(){
+        return{
+          imgUrl: null, //截图地址
+          swiperOption:{
+            direction:"horizontal",
+            pagination:{
+              el: '.swiper-pagination',
+              clickable:true
+                },
+            navigation: {
+              nextEl: '.swiper-button-next',
+              revEl: '.swiper-button-prev'
+        }
+            }
+        }
+    },
+     methods: {
     //获取截图方法
     getPrintScreen() {
       html2canvas(this.$refs.imageDom,
       {
         imageTimeout: 15000, //newline
-        scale:3, //newline
+        dpi: 300, // 处理模糊问题
         useCORS: true, //图片跨域，开启跨域配置
         logging: false,//日志开关，便于查看html2canvas的内部执行流程
         taintTest: true,//是否在渲染前测试图片
       }).then(canvas => {
         // 转成图片，生成图片地址
-        let imgUrl = canvas.toDataURL("image/png",1.0); //可将 canvas 转为 base64 格式
+        let imgUrl = canvas.toDataURL("image/png",1); //可将 canvas 转为 base64 格式
         let eleLink = document.createElement("a");
         eleLink.href = imgUrl; // 转换后的图片地址
-        // eleLink.download = "名称"
-        FileSaver.saveAs(imgUrl, 'sss.png')
+        // eleLink.download = "名称";
+        FileSaver.saveAs(imgUrl, 'ak.png')
         document.body.appendChild(eleLink);
         eleLink.click();
         document.body.removeChild(eleLink);
-      },300);
+      });
     }
   },
-};
+
+}
 </script>
 
-
-<!-- 本demo样式代码（不重要） -->
 <style scoped>
-.content {
-    height: 30%;
-    font-size: 26px;
-    font-family: Source Han Sans CN;
-    color: rgb(245, 245, 245);
-    text-align: center;
-    line-height: 200px;
-    background-color: rgb(243, 161, 152);
-  }
-  .btn {
-    display: block;
-    width: 80vw;
-    height: 11.733vw;
-    background: #79c76f;
-    border-radius: 2vw;
-    border: none;
-    font-size: 4.8vw;
-    font-family: Source Han Sans CN;
-    font-weight: 400;
-    color: #ffffff;
-    line-height: 11.733vw;
-    letter-spacing: 4px;
-    text-align: center;
-    margin: 30px 0 20px 40px;
-  }
-#page {
-  padding: 0;
-  width: 100%;
-  height: 100%;
-  background-color: #fff;
+
+.body{
+    width: 100%;
+    height: 100%;
+    position: absolute;
+    left: 0; 
+    top: 0;
+}
+.swiper-slide{
+    width: 320px;
+    height: 600px;
+    touch-action: none;
+    background: green;
+     /* position: relative;
+     margin-left: 100px;
+     margin-top: 150px;
+     touch-action: none; */
+}
+.childSwiper{
+    font-family: Helvetica, Tahoma, Arial, 'PingFang SC', 'Hiragino Sans GB', 'Heiti SC', STXihei, 'Microsoft YaHei'; /* 根节点增加字体设置，避免iOS 15生成图片失败 */
+}
+.one{
+  background: url('../assets/bg.jpg');
 }
 </style>
