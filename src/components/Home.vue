89286064adc13fec7f8784f033d49cf00160acaa
z990814 <67317862+z990814@users.noreply.github.com>
2022-12-16 16:18:00 +0800

    首页

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -1,7 +1,8 @@
 <template>
  <div class="body">
    <div class="container">
-        <a href="#" class="pulse"></a>
+        <a href="#" class="pluse"></a>
+        <p>点击查看</p>
   </div>
   </div>
 </template>
@@ -18,5 +19,55 @@ export default {
     background-size: cover;
     width: 100%;
     height: 100vh;
+    
   }
+  .container{
+    position: absolute;
+    padding-top: 80vh;
+    margin-left: 160px;
+  
+  }
+  .container p{
+    padding-top: 8vh;
+    color: white;
+    margin-left: -8px;
+  }
+  .pluse{
+    position: absolute;
+    padding: 7px;
+    width: 2rem;
+    color: #fff;
+    transition: all .5s ease;
+    background: rgb(255,215,0);
+    /* top: 50%;
+    right: 50%;
+    margin-right: 50%;
+    margin-top: 50%; */
+  }
+  .pluse,
+  .pluse::before,
+  .pluse::after{
+    content: "";
+    display: grid;
+    grid-area: 1/1;
+    aspect-ratio: 1;
+    border-radius: 50%;
+    box-shadow: 0 0 0 0 #d9ea18c0;
+    animation: plusing 3s linear infinite var(--s,0s);
+  }
+  .pluse::before{
+    --s:1s
+  }
+  .pluse::after{
+    --s:2s
+  }
+  .pluse:hover{
+    background: #35D1E633;
+    color: #ffffff99;
+  }
+@keyframes plusing {
+    to{
+        box-shadow: 0 0 0 2rem #0000;
+    }
+}
 </style>
