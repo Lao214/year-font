6dad6b9b450dd9ba85f37756d21c131dc4788a16
z990814 <67317862+z990814@users.noreply.github.com>
2022-12-19 14:42:30 +0800

    删除默认工号

--- src/components/PageFour.vue
+++ src/components/PageFour.vue
@@ -1,8 +1,8 @@
 <template>
    <div class="body">
    <div class="container">
-    <p class="flip-horizontal-top" >你喜欢用<a style="font-size: 2rem;color: rgb(255,215,0);">{{dataObj.device}}</a>登陆富学宝典平台学习</p>
-    <p class="flip-horizontal-top">你今年学习最用功的是<a style="font-size: 2rem;color: rgb(255,215,0);">{{dataObj.mon}}</a>月，超越了<a style="font-size: 2rem;color: rgb(255,215,0);">{{dataObj.rate2}}%</a>的小伙伴</p>
+    <p class="flip-horizontal-top" >你喜欢用<a style="font-size: 2rem;color: rgb(255,215,0);">{{dataObj.device}}</a>登陆富学宝典平台学习。</p>
+    <p class="flip-horizontal-top">你今年学习最用功的是<a style="font-size: 2rem;color: rgb(255,215,0);">{{dataObj.mon}}</a>月，超越了<a style="font-size: 2rem;color: rgb(255,215,0);">{{dataObj.rate2}}%</a>的小伙伴。</p>
     <p class="flip-horizontal-top" style="line-height:10px;">你喜欢在<a style="font-size: 2rem;color: rgb(255,215,0);">{{dataObj.time}}:00</a>开始你的学习！</p>
   </div>
   </div>
