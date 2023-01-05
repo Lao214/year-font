f8ad087a8af283fcd39504ca868aa4c28f076cce
Lao214 <112752790+Lao214@users.noreply.github.com>
2023-01-05 17:19:52 +0800

    sunshine数据

---
+++ src/api/sunshine.js
@@ -0,0 +1,15 @@
+import request from '@/utils/ request'
+
+// 常量
+const api_name = '/year/sunshine'
+
+export default {
+   // 列表
+   getDataByJobNo(jobNo) {
+    return request({
+      // 接口路径
+      url: `${api_name}/getOneByJobNo/${jobNo}`,
+      method: 'get', // 提交方式
+    })
+  }
+}
